d06cc404a0661316eb6e6ac59af93db806c0b7c5
Vitalie <vitalieungureanu200@gmail.com>
2024-02-15 16:02:19 +0100

    principio servicio paypal, cambio endpoint provincias y munipios para hacerlo con axios desde geoapi

--- config_server_express/routingTienda.js
+++ config_server_express/routingTienda.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router(); //objeto router de express para definir endpoints de la zona cliente
 const tiendaController = require("../controllers/tiendaController");
-router.get('/RecuperarCategorias:idcat', tiendaController.recuperarCategorias);
-router.get('/RecuperarLibros:idcat', tiendaController.recuperarLibros);
-router.get('/RecuperarLibro:isbn13', tiendaController.recuperarLibro);
+router.get('/RecuperarLibros', tiendaController.recuperarLibros); //<---- en url, hay variable: ?email=....
+router.get('/RecuperarUnLibro', tiendaController.recuperarUnLibro);
+router.get('/RecuperarCategorias', tiendaController.recuperarCategorias);
+router.get('/RecuperarProvincias', tiendaController.recuperarProvincias);
+router.get('/RecuperarMunicipios', tiendaController.recuperarMunicipios);
 
 
 
--- controllers/tiendaController.js
+++ controllers/tiendaController.js
@@ -1,66 +1,113 @@
 const { initializeApp } = require("firebase/app");
 const app = initializeApp(JSON.parse(process.env.FIREBASE_CONFIG));
 const { getAuth } = require("firebase/auth");
+const axios = require('axios');
 const auth = getAuth(app);
 
-const {
-  getFirestore,
-  getDocs,
-  collection,
-  query,
-  where,
-} = require("firebase/firestore");
+const {getFirestore, getDocs, collection, where, query, addDoc, getDoc, orderBy, startAt}=require('firebase/firestore');
+
 const db = getFirestore(app);
 
+
+
 module.exports = {
-  recuperarCategorias: async function (req, res, next) {
-    try {
-      console.log("idcategoria recibida desde react...", req.params.idcat);
-      var _idcategoria = req.params.idCategoria;
-      let _categorias = [];
-      var _patron =
-        _idcategoria === "path"
-          ? new RegExp("^\\d{1,}$")
-          : new RegExp("^" + _idcategoria + "-\\d{1,}$");
-      const _cats = await getDocs(collection(db, "categorias"));
-      _cats.forEach((doc) => {
-        if (_patron.test(doc.data().IdCategoria)) {
-          _categorias.push(doc.data());
+    recuperarProvincias: async (req,res,next)=>{
+        try {
+            let _resp=await axios.get(`https://apiv1.geoapi.es/provincias?type=JSON&key=${process.env.GEOAPI_KEY}&sandbox=0`);
+            let _provs=_resp.data.data;
+            //console.log('provincias recuperadas...', _provs);            
+
+            /* ----- con coleccion provincias firebase....
+            let _snapProvs=await getDocs(collection(db,'provincias'),orderBy('PRO'));            
+            
+            let _provs=[];
+            _snapProvs.forEach( snapProv=> _provs.push(snapProv.data()));
+            */
+
+            res.status(200).send(_provs);
+
+        } catch (error) {
+            console.log('error al recuperar provincias...',error);
+            res.status(400).send([]);        
         }
-      });
-      res.status(200).send(_categorias);
-    } catch (error) {
-      console.log("error al recuperar categorias...", error);
-      res.status(500).send([]);
+    },
+    recuperarMunicipios: async (req,res,next)=>{
+        try {
+            let _codpro=req.query.codpro;
+
+            let _resp=await axios.get(`https://apiv1.geoapi.es/municipios?CPRO=${_codpro}&type=JSON&key=${process.env.GEOAPI_KEY}&sandbox=0`);
+            let _munis=_resp.data.data;
+            //console.log(`municipios de provincia ${_codpro}...`, _munis);
+
+            /* ---------- desde firebase en coleccion municipios ....
+            let _snapMunis=await getDocs(collection(db,'municipios'),where('CPRO','==',_codpro),orderBy('DMUN50'));
+            
+            let _munis=[];
+            _snapMunis.forEach( snapMuni=>_munis.push(snapMuni.data()));
+
+            console.log('municipios recuperados...', _munis);
+            */
+           
+
+        } catch (error) {
+            console.log('error al recuperar municipios...',error);
+            res.status(400).send([]);
+        }
+    },
+    recuperarLibros: async (req,res,next)=>{
+        try {
+            let _idcat=req.query.idcat;
+            console.log('recuperando libros de categoria...', _idcat);
+            //firebase NO PUEDE BUSCAR POR PATRONES dentro de un campo de texto, como cualquier otra bd...solucion? o bajas toda la coleccion de libros y buscas del lado del cliente
+            //o buscas api externas de busqueda de texto: https://firebase.google.com/docs/firestore/solutions/search?provider=algolia
+            //muy usadas como algolia,elastic,...
+            let _snapshotibros=await getDocs(query(collection(db,'libros'),orderBy('IdCategoria'),startAt(_idcat)));
+            let _libros=[];
+            _snapshotibros.forEach( snaplibro=> _libros.push(snaplibro.data()));
+
+            res.status(200).send(_libros);
+                
+        } catch (error) {
+            console.log('error al recuperar libros...',error);
+            res.status(400).send([]);
+
+        }        
+    },
+    recuperarUnLibro: async (req,res,next)=>{
+        try {
+            let _isbn=req.query.isbn;
+            console.log('recuperando libro con isbn...', _isbn);
+    
+            let _librosnaps=await getDocs( query(collection(db,'libros'),where('ISBN13','==',_isbn)) );
+            let _libro={};
+            _librosnaps.forEach( librosnap=> _libro=librosnap.data());
+    
+            res.status(200).send(_libro);    
+
+        } catch (error) {
+            console.log('error al recuperar libro por isbn...',error);
+            res.status(400).send(undefined);
+        }
+    },
+    recuperarCategorias: async (req,res,next)=>{
+        try {
+            let _idcat=req.query.idcat;
+            let _regex;
+            if(_idcat=="raices") {
+                _regex=new RegExp("^[0-9]{1,}$");
+            } else {
+                _regex=new RegExp("^" + _idcat + "-[0,9]{1,}$")
+            }
+            let _catSnaps=await getDocs(collection(db,'categorias'));
+            
+            let _cats=[];
+            _catSnaps.forEach( catdoc => _cats.push(catdoc.data()));
+
+            res.status(200).send(_cats.filter( cat=> _regex.test(cat.IdCategoria) ).sort( (a,b)=>parseInt(a.IdCategoria) < parseInt(b.IdCategoria) ? -1 : 1 ));
+
+        } catch (error) {
+            console.log('error recuperar categorias...',error);
+            res.status(400).send([]);
+        }
     }
-  },
-  recuperarLibros: async function (req, res, next) {
-    try {
-      const idcat = req.params.idcat;
-      console.log("idcat recibido desde react...", idcat);
-      const _libros = [];
-      const _q = query(
-        collection(db, "libros")
-      );
-      const _querySnapshot = await getDocs(_q);
-      _querySnapshot.forEach((doc) => {
-        _libros.push(doc.data());
-      });
-      const _patron=idcat==='padres' ?  new RegExp("^\\d{1,}$") : new RegExp("^" + idcat + "-\\d{1,}$");
-      const libroFiltrador = _libros.filter((libro) => {
-        return _patron.test(libro.IdCategoria);
-      });
-      res.status(200).send(_libros);
-    } catch (error) {
-      console.log("Error al enviar libros", error);
-      res.status(500).send([]);
-    }
-  },
-  recuperarLibro: async function (req, res, next) {
-    try {
-    } catch (error) {
-      console.log("Error al enviar libro", error);
-      res.status(500).send(error);
-    }
-  },
 };
